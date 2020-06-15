6932a1d613a81a6a041ebec664f592c625410f57
KooHyongMo <chrisais9@naver.com>
2020-06-15 20:23:51 +0900

    Initial Commit

---
+++ app/src/main/java/kr/koohyongmo/lockkeyboard_client/MainActivity.kt
@@ -0,0 +1,20 @@
+package kr.koohyongmo.lockkeyboard_client
+
+import android.inputmethodservice.Keyboard
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val mCustomKeyboard: LockKeyboardView =
+                findViewById<View>(R.id.keyboardview) as LockKeyboardView
+        mCustomKeyboard.keyboard = Keyboard(this, R.xml.numkbd)
+        mCustomKeyboard.isPreviewEnabled = false // NOTE Do not show the preview balloons
+        mCustomKeyboard.registerEditText(R.id.edittext1)
+//        mCustomKeyboard.setAllignBottomCenter(true)
+    }
+}
---
+++ app/src/main/java/kr/koohyongmo/lockkeyboard_client/utils/CalculateUtil.kt
@@ -0,0 +1,16 @@
+package kr.koohyongmo.lockkeyboard_client.utils
+
+import android.content.Context
+import android.util.DisplayMetrics
+
+/**
+ * Created by KooHyongMo on 2020/06/15
+ */
+object CalculateUtil {
+
+    fun dp2px(context: Context, dp: Float): Float {
+        val resources = context.resources
+        val metrics = resources.displayMetrics
+        return dp * (metrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
+    }
+}
